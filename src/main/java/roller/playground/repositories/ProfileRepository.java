e680f5112fef87719c242b58dcafb3389025817d
Roller Lo <jasonbh02@gmail.com>
2026-02-02 10:34:43 +0800

    perf(repositories): Add EntityGraph to optimize user fetching to avoid n+1 problem in ProfileRepository

--- src/main/java/roller/playground/repositories/ProfileRepository.java
+++ src/main/java/roller/playground/repositories/ProfileRepository.java
@@ -1,10 +1,12 @@
 package roller.playground.repositories;
 
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.repository.CrudRepository;
 import roller.playground.entities.Profile;
 
 import java.util.List;
 
 public interface ProfileRepository extends CrudRepository<Profile, Long> {
+    @EntityGraph(attributePaths = "user")
     List<Profile> findProfilesByLoyaltyPointsGreaterThan(Integer loyaltyPoints);
 }
