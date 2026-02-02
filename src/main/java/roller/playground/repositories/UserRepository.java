da4cf466da27eebd3936e7d182f0f994d7d41ded
Roller Lo <jasonbh02@gmail.com>
2026-02-02 11:40:18 +0800

    refactor(repositories, services): Use findByLoyaltyPoints in UserRepository and update UserService to leverage simplified projection

--- src/main/java/roller/playground/repositories/UserRepository.java
+++ src/main/java/roller/playground/repositories/UserRepository.java
@@ -3,6 +3,8 @@ package roller.playground.repositories;
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+import roller.playground.dtos.UserSummary;
 import roller.playground.entities.User;
 
 import java.util.List;
@@ -15,4 +17,7 @@ public interface UserRepository extends CrudRepository<User, Long> {
     @EntityGraph(attributePaths = "addresses")
     @Query("select u from User u")
     List<User> findAllWithAddresses();
+
+    @Query("select u.id as id, u.email as email from User u where u.profile.loyaltyPoints > :points order by u.email")
+    List<UserSummary> findByLoyaltyPoints(@Param("points") Integer points);
 }
--- src/main/java/roller/playground/services/UserService.java
+++ src/main/java/roller/playground/services/UserService.java
@@ -67,7 +67,7 @@ public class UserService {
 
     @Transactional
     public void fetchProfile() {
-        var profiles = profileRepository.findByLoyaltyPoints(2);
+        var profiles = userRepository.findByLoyaltyPoints(2);
         profiles.forEach(p -> {
             System.out.println(p);
             System.out.println(p.getId());
